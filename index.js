62457422fe93befee37e6aab054a78daebc964b8
Michał Kornela <95033551+TheM-Dev@users.noreply.github.com>
2022-10-24 17:54:25 +0200

    Rework needed

--- index.js
+++ index.js
@@ -13,23 +13,36 @@ module.exports = function(options){
          * 
          * @returns { Object }
          */
-        platformStatus = async region => {
+        platformStatus: async region => {
             let url = `${this.baseUri(region)}lol/status/v4/platform-data?api_key=${this.apiKey}`;
             const res = await axios.get(url).then((response) => {
-                const { maintenances, incidents } = response;
+                let incidents = response.data.incidents;
+                let maintenances = response.data.maintenances;
                 const maintenanceReports = [];
                 const incidentReports = [];
-                incidents.forEach(i => {
+                Array.from(incidents).forEach(i => {
                     const { id, platforms, created_at, updates, incident_severity } = i;
                     const updateReports = [];
-                    updates.forEach(u => {
+                    Array.from(updates).forEach(u => {
                         const { id, publish_locations, author, created_at, translations, titles } = u;
                         let title = ''; let translation = '';
-                        translations.forEach(trans => { if(trans.locale === 'en_US') return translation = trans.content });
-                        titles.forEach(title => { if(title.locale === 'en_US') return title = title.content });
+                        if(translations) Array.from(translations).forEach(trans => { if(trans.locale === 'en_US') return translation = trans.content });
+                        if(titles) Array.from(titles).forEach(title => { if(title.locale === 'en_US') return title = title.content });
                         updateReports.push({ id, publish_locations, author, created_at, translation, title });
                     });
                     incidentReports.push({ id, platforms, created_at, updateReports, incident_severity });
+                });
+                Array.from(maintenances).forEach(i => {
+                    const { id, platforms, created_at, updates, incident_severity } = i;
+                    const updateReports = [];
+                    Array.from(updates).forEach(u => {
+                        const { id, publish_locations, author, created_at, translations, titles } = u;
+                        let title = ''; let translation = '';
+                        if(translations) Array.from(translations).forEach(trans => { if(trans.locale === 'en_US') return translation = trans.content });
+                        if(titles) Array.from(titles).forEach(title => { if(title.locale === 'en_US') return title = title.content });
+                        updateReports.push({ id, publish_locations, author, created_at, translation, title });
+                    });
+                    maintenanceReports.push({ id, platforms, created_at, updateReports, incident_severity });
                 });
                 return { maintenanceReports, incidentReports };
             }).catch((err) => {
